b08cacdb2c34d3ab804ea13631f19283c7b62993
Sven Wappler <info@wappler.systems>
2024-04-07 00:37:50 +0200

    clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,1 @@
 <?php
-
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\TYPO3\CMS\Backend\Form\FormDataProvider\TcaTag::class] = [
-    'depends' => [
-        \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectItems::class
-    ]
-];
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,12 +1,12 @@
-
 #
 # Table structure for table 'sys_tag'
 #
-CREATE TABLE sys_tag (
-	title tinytext NOT NULL,
-	items int(11) DEFAULT '0' NOT NULL,
+CREATE TABLE sys_tag
+(
+    title tinytext            NOT NULL,
+    items int(11) DEFAULT '0' NOT NULL,
 
-	KEY category_list (pid,deleted,sys_language_uid)
+    KEY category_list (pid, deleted, sys_language_uid)
 );
 
 CREATE TABLE tt_content
